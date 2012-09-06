45c8d3fb727e00ac2a9de3a9fc83f4500b981d55
varac <varacanero@zeromail.org>
2012-09-06 14:23:17 +0200

    install ruby-hiera-puppet

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,19 @@ apt-get install lsb-release git
 
 # we need puppet from backports
 dist="`lsb_release -cs`"
+
+# enable backports for puppet + facter
 [ -f /etc/apt/sources.list.d/$dist-backports.list ] || echo "deb http://backports.debian.org/debian-backports/ $dist-backports main contrib non-free">/etc/apt/sources.list.d/$dist-backports.list
+
+# enable debian testing for ruby-hiera-puppet
+cat > /etc/apt/preferences.d/wheezy <<DELIM
+Package: *
+Pin: release o=Debian,n=wheezy
+Pin-Priority: 2
+DELIM
 
 apt-get update
 apt-get install -y -t $dist-backports facter puppet
+apt-get install ruby-hiera-puppet ruby-hiera
 
 puppet apply --modulepath=$PWD/puppet/modules $PWD/puppet/manifests/site.pp $@
