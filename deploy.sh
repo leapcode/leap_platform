653efcee3f3427817e63a8432df99c1e932e3261
varac <varacanero@zeromail.org>
2012-09-06 11:46:46 +0200

    install puppet+facter from backports

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+# 
+# missing: header, licence, usage
+
+
+apt-get install lsb-release git 
+
+# we need puppet from backports
+dist="`lsb_release -cs`"
+[ -f /etc/apt/sources.list.d/$dist-backports.list ] || echo "deb http://backports.debian.org/debian-backports/ $dist-backports main contrib non-free">/etc/apt/sources.list.d/$dist-backports.list
+
+apt-get update
+apt-get install -t $dist-backports facter puppet
 
 puppet apply --modulepath=$PWD/puppet/modules $PWD/puppet/manifests/site.pp $@
