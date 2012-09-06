2c2e3608a251bdb8210767484e05c896f6803d6c
varac <varacanero@zeromail.org>
2012-09-06 11:29:17 +0200

    beginning of openvpn server config

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,15 @@
-node "default" {
-  notify {'Hello World':}
+node 'cougar.leap.se' {
+  openvpn::server {
+    'cougar.leap.se':
+        country      => 'TR',
+        province     => 'Ankara',
+        city         => 'Ankara',
+        organization => 'leap.se',
+        email        => 'sysdev@leap.se';
+}
+
+}
+
+node 'default' {
+  notify {'Please specify a host in site.pp!':}
 }
