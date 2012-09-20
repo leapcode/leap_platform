764ae6f21a8a54af78b29fc14876af36e2dd4651
varac <varacanero@zeromail.org>
2012-09-20 13:39:23 +0200

    parse new config layout

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,10 +1,22 @@
+define print() {
+   notice("The value is: '${name}'")
+}
+ 
+
 node 'default' {
-  $service='eip'
-  $password=hiera('testpw')
-  $openvpn_ports=hiera_array('openvpn_ports')
-  $tor=hiera('tor')
-  notify {"Password: $password":}
-  notify {"Openvpn Config for $fqdn: openvpn_ports=$openvpn_ports, tor=$tor":}
-  #include site_openvpn
+  #$password=hiera('testpw')
+  #notify {"Password: $password":}
+
+  $services=hiera_array('services')
+  notice("Services for $fqdn: $services")
+
+  if 'eip' in $services {
+    $openvpn_ports=hiera_array('openvpn_ports')
+    $tor=hiera('tor')
+    notice("Openvpn Config for $fqdn: openvpn_ports=$openvpn_ports, tor=$tor")
+    print{$openvpn_ports:}
+    #include site_openvpn
+  }
+
 
 }
