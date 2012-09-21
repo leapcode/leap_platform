276de1e249b25e5e00c49229132215681aee6467
varac <varacanero@zeromail.org>
2012-09-21 20:26:20 +0200

    basic configuration for openvpn server files

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,19 +1,18 @@
-define print() {
-  notice("The value is: '${name}'")
-}
-
 node 'default' {
-  $concat_basedir =  '/var/lib/puppet/modules/concat'
+  # $concat_basedir =  '/var/lib/puppet/modules/concat'  # do we need this ?
   include concat::setup
 
   $services=hiera_array('services')
   notice("Services for $fqdn: $services")
 
   if 'eip' in $services {
+    include site_openvpn
+
     $tor=hiera('tor')
     notice("Tor enabled: $tor")
 
-    $openvpn_config=hiera('openvpn')
-    create_resources('site_openvpn::server_config', $openvpn_config)
+    $openvpn_configs=hiera('openvpn_server_configs')
+    create_resources('site_openvpn::server_config', $openvpn_configs)
+
   }
 }
