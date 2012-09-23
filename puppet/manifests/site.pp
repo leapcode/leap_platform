8320de2fd5bd8fcb429dfc1b68527a1c39a8341f
varac <varacanero@zeromail.org>
2012-09-23 19:02:28 +0200

    reorderd config, include site_sshd

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -5,6 +5,14 @@ node 'default' {
   $services=hiera_array('services')
   notice("Services for $fqdn: $services")
 
+  # configure ssh and inculde ssh-keys
+  #include sshd
+  $ssh_keys=hiera_hash('ssh_keys')
+  include site_sshd
+  notice($ssh_keys)
+  create_resources('site_sshd::ssh_key', $ssh_keys)
+
+
   if 'eip' in $services {
     include site_openvpn
 
