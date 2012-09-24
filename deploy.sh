e6651283ac7b7c4114224e90936e5cf26f4b8c65
varac <varacanero@zeromail.org>
2012-09-24 18:41:16 +0200

    handle submodules

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,18 @@ PUPPET_ENV='--confdir=puppet'
 
 install_prerequisites () {
   apt-get update
-  apt-get install puppet
+  apt-get install puppet git
 
   # lsb is needed for a first puppet run
   puppet apply $PUPPET_ENV --execute 'include lsb'
+  git submodule init
+  git submodule update
 }
 
 # main 
 
 # commented for testing purposes
-#install_prerequisites
+install_prerequisites
 
 puppet apply $PUPPET_ENV puppet/manifests/site.pp $@
 
