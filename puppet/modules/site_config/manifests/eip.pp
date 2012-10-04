b49ab6a1a06bcc31984e09a5371510643eef3c87
varac <varacanero@zeromail.org>
2012-10-04 23:55:03 +0200

    use different parameter  for each config

--- puppet/modules/site_config/manifests/eip.pp
+++ puppet/modules/site_config/manifests/eip.pp
@@ -9,17 +9,19 @@ class site_config::eip {
   #create_resources('site_openvpn::server_config', $openvpn_configs)
  
   site_openvpn::server_config { 'tcp_config':
-    port    => '1194',
-    proto   => 'tcp',
-    local   => $::ipaddress_eth0_1,
-    server  => '10.42.0.0 255.255.248.0',
-    push    => '"dhcp-option DNS 10.42.0.1"',
+    port        => '1194',
+    proto       => 'tcp',
+    local       => $::ipaddress_eth0_1,
+    server      => '10.1.0.0 255.255.248.0',
+    push        => '"dhcp-option DNS 10.1.0.1"',
+    management  => 'management 127.0.0.1 1000'
   }
   site_openvpn::server_config { 'udp_config':
-    port    => '1194',
-    proto   => 'udp',
-    local   => $::ipaddress_eth0_1,
-    server  => '10.43.0.0 255.255.248.0',
-    push    => '"dhcp-option DNS 10.43.0.1"',
+    port        => '1194',
+    proto       => 'udp',
+    local       => $::ipaddress_eth0_1,
+    server      => '10.2.0.0 255.255.248.0',
+    push        => '"dhcp-option DNS 10.2.0.1"',
+    management  => 'management 127.0.0.1 1001'
   }
 }
