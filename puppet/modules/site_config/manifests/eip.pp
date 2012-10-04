14305e553c4f71fbeec997d585383c4c6211c1a5
varac <varacanero@zeromail.org>
2012-10-04 17:29:26 +0200

    don't pull openvpn config from hiera

--- puppet/modules/site_config/manifests/eip.pp
+++ puppet/modules/site_config/manifests/eip.pp
@@ -4,7 +4,15 @@ class site_config::eip {
   $tor=hiera('tor')
   notice("Tor enabled: $tor")
 
-  $openvpn_configs=hiera('openvpn_server_configs')
-  create_resources('site_openvpn::server_config', $openvpn_configs)
-
+  #$openvpn_configs=hiera('openvpn_server_configs')
+  #create_resources('site_openvpn::server_config', $openvpn_configs)
+  
+  site_openvpn::server_config { 'tcp_config':
+    port => '1194',
+    proto => 'tcp'
+  }
+  site_openvpn::server_config { 'udp_config':
+    port => '1194',
+    proto => 'udp'
+  }
 }
