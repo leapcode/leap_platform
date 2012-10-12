0eff2049fa8d846dffee3236824b8bc42e581467
varac <varacanero@zeromail.org>
2012-10-12 15:32:15 +0200

    added ruby-hiera-puppet as dependency

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,28 @@
 PUPPET_ENV='--confdir=puppet'
 
 install_prerequisites () {
-  apt-get update
-  apt-get install puppet git
+  PACKAGES='git puppet ruby-hiera-puppet'
+  dpkg -l $PACKAGES > /dev/null 2>&1
+  if [ ! $? -eq 0 ]
+  then 
+    apt-get update
+    apt-get install $PACKAGES 
+  fi
 
   # lsb is needed for a first puppet run
   puppet apply $PUPPET_ENV --execute 'include lsb'
-  git submodule init
-  git submodule update
 }
 
 # main 
 
 # commented for testing purposes
+# this should be run once on every host on setup
 #install_prerequisites
 
-puppet apply $PUPPET_ENV puppet/manifests/site.pp $@
+# keep repository up to date
+git pull
+git submodule init
+git submodule update
 
+# run puppet without irritating deprecation warnings
+puppet apply $PUPPET_ENV puppet/manifests/site.pp $@ | grep -v 'warning:.*is deprecated'
