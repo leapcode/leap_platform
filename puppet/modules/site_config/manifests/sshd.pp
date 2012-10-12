082efdddf4b5a4c741a655e6833b8d86bb717303
varac <varacanero@zeromail.org>
2012-10-12 14:44:05 +0200

    ssh_keys -> ssh_pubkeys for clarity

--- puppet/modules/site_config/manifests/sshd.pp
+++ puppet/modules/site_config/manifests/sshd.pp
@@ -1,8 +1,8 @@
 class site_config::sshd {
   # configure ssh and inculde ssh-keys
   include sshd
-  $ssh_keys=hiera_hash('ssh_keys')
+  $ssh_pubkeys=hiera_hash('ssh_pubkeys')
   include site_sshd
-  notice($ssh_keys)
-  create_resources('site_sshd::ssh_key', $ssh_keys)
+  notice($ssh_pubkeys)
+  create_resources('site_sshd::ssh_key', $ssh_pubkeys)
 }
