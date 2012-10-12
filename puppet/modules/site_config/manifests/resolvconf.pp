b297dd3c47a9d23eaba6070555ecec47f3acbcc6
varac <varacanero@zeromail.org>
2012-10-12 15:09:40 +0200

    add third dns server (swiss privacy found.)

--- puppet/modules/site_config/manifests/resolvconf.pp
+++ puppet/modules/site_config/manifests/resolvconf.pp
@@ -6,11 +6,12 @@ class site_config::resolvconf {
   $domain_hash = hiera('domain')
   $domain_public = $domain_hash['public']
 
-  # 127.0.0.1: caching-only local bind
+  # 127.0.0.1:      caching-only local bind
   # 87.118.100.175: http://server.privacyfoundation.de
+  # 62.141.58.13:   http://www.privacyfoundation.ch/de/service/server.html
   class { '::resolvconf':
     domain      => $domain_public,
     search      => $domain_public,
-    nameservers => [ '127.0.0.1', '87.118.100.175' ]
+    nameservers => [ '127.0.0.1', '87.118.100.175', '62.141.58.13' ]
   }
 }
