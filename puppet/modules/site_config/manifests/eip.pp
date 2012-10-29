c26c2c18d0abb7dec76a748bf0c2c2f9000298da
varac <varacanero@zeromail.org>
2012-10-29 22:17:26 +0100

    openvpn_tcp/udp_network_prefix and openvpn_tcp/udp_netmask variables

--- puppet/modules/site_config/manifests/eip.pp
+++ puppet/modules/site_config/manifests/eip.pp
@@ -2,26 +2,30 @@ class site_config::eip {
   include site_openvpn
   include site_openvpn::keys
 
-  $ip_address               = hiera('ip_address')
-  $interface                = hiera('interface')
-  #$gateway_address          = hiera('gateway_address')
-  $openvpn_config           = hiera('openvpn')
-  $openvpn_gateway_address  = $openvpn_config['gateway_address']
+  $ip_address                 = hiera('ip_address')
+  $interface                  = hiera('interface')
+  #$gateway_address           = hiera('gateway_address')
+  $openvpn_config             = hiera('openvpn')
+  $openvpn_gateway_address    = $openvpn_config['gateway_address']
+  $openvpn_tcp_network_prefix = '10.1.0'
+  $openvpn_tcp_netmask        = '255.255.248.0'
+  $openvpn_udp_network_prefix = '10.2.0'
+  $openvpn_udp_netmask        = '255.255.248.0'
 
   site_openvpn::server_config { 'tcp_config':
     port        => '1194',
     proto       => 'tcp',
     local       => $openvpn_gateway_address,
-    server      => '10.1.0.0 255.255.248.0',
-    push        => '"dhcp-option DNS 10.1.0.1"',
+    server      => "$openvpn_tcp_network_prefix.0 $openvpn_tcp_netmask",
+    push        => "\"dhcp-option DNS $openvpn_tcp_network_prefix.1\"",
     management  => '127.0.0.1 1000'
   }
   site_openvpn::server_config { 'udp_config':
     port        => '1194',
     proto       => 'udp',
+    server      => "$openvpn_udp_network_prefix.0 $openvpn_udp_netmask",
+    push        => "\"dhcp-option DNS $openvpn_udp_network_prefix.1\"",
     local       => $openvpn_gateway_address,
-    server      => '10.2.0.0 255.255.248.0',
-    push        => '"dhcp-option DNS 10.2.0.1"',
     management  => '127.0.0.1 1001'
   }
 
