69ba8553f483d99782775e8ed5ab01cd45a75e72
varac <varacanero@zeromail.org>
2012-10-30 16:01:41 +0100

    configure unstable pinning for couchdb before install

--- puppet/modules/site_couchdb/manifests/init.pp
+++ puppet/modules/site_couchdb/manifests/init.pp
@@ -1,18 +1,5 @@
 class site_couchdb {
 
-  # for now, we need to install couchdb from unstable,
-  # because of this bug while installing:
-  # http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=681549
-  # can be removed when couchdb/1.2.0-2 is integrated into testing
-  apt::sources_list { 'unstable.list':
-    source => [ 'puppet:///modules/site_apt/unstable.list'],
-  }
-  apt::preferences_snippet{
-    'couchdb': release => "unstable", priority => 999;
-  }
-
-  class { 'couchdb':
-    #bind => '0.0.0.0'
-  }
+  class {'site_couchdb::package':} -> class {'site_couchdb::configure':}
 
 }
