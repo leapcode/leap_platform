ffc0bba5390b30093b0cfdf9f927ba1f7db66ee8
varac <varacanero@zeromail.org>
2012-11-01 09:15:54 +0100

    another try of class relationships

--- puppet/modules/site_couchdb/manifests/configure.pp
+++ puppet/modules/site_couchdb/manifests/configure.pp
@@ -1,7 +1,7 @@
 class site_couchdb::configure {
-  #Class[site_couchdb::package] -> Class[site_couchdb::configure] 
+  Class[site_couchdb::package] -> Class[couchdb] 
   class { 'couchdb':
+    require => Class['site_couchdb::package']
     #bind => '0.0.0.0'
   }
-
 }
--- puppet/modules/site_couchdb/manifests/init.pp
+++ puppet/modules/site_couchdb/manifests/init.pp
@@ -1,10 +1,10 @@
 class site_couchdb {
 
   # install couchdb package first, then configure it
-  
-  Class[site_couchdb::package] -> Class[site_couchdb::configure]
+  Class['site_couchdb::package'] -> Class['site_couchdb::configure']
 
   include site_couchdb::package
   include site_couchdb::configure
 
+  include couchdb::deploy_config
 }
