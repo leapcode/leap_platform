9f7a64ab2813e2c475a776efff4ad9a380ca6cc1
varac <varacanero@zeromail.org>
2012-11-01 10:09:37 +0100

    deploy ssl cert working

--- puppet/modules/site_couchdb/manifests/init.pp
+++ puppet/modules/site_couchdb/manifests/init.pp
@@ -1,11 +1,19 @@
 class site_couchdb {
 
+  $couchdb_config = hiera('couchdb')
+  $key            = $couchdb_config['key']
+  $cert           = $couchdb_config['crt']
+
   # install couchdb package first, then configure it
   Class['site_couchdb::package'] -> Class['site_couchdb::configure']
 
   include site_couchdb::package
   include site_couchdb::configure
 
-  include couchdb::ssl::deploy_certs
+
+  couchdb::ssl::deploy_cert { 'cert':
+    key  => $key,
+    cert => $cert,
+  }
   include couchdb::deploy_config
 }
