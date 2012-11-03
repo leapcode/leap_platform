5981a73edce0a64f26bb8abb799c180b856abbbd
varac <varacanero@zeromail.org>
2012-11-03 11:43:58 +0100

    overwrite /etc/apache2/ports.conf so 0-default.conf and 0-default_ssl.conf don't start on port 80/443

--- puppet/modules/site_couchdb/manifests/init.pp
+++ puppet/modules/site_couchdb/manifests/init.pp
@@ -16,12 +16,20 @@ class site_couchdb {
   #  key  => $key,
   #  cert => $cert,
   #}
-  
+
   include apache::ssl
-  apache::module { 
-    'rewrite': ensure => present;
-    'proxy':   ensure => present; 
-    'proxy_http':   ensure => present; 
-   }
+  apache::module {
+    'rewrite':      ensure => present;
+    'proxy':        ensure => present;
+    'proxy_http':   ensure => present;
+  }
   apache::vhost::file { 'couchdb_proxy': }
+  # prevent 0-default.conf and 0-default_ssl.conf from apache module
+  # from starting on port 80 / 443
+  file { '/etc/apache2/ports.conf':
+    content => '',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
 }
