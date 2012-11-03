82c21f345c78c4f06e4aa78ab6020f1393816812
varac <varacanero@zeromail.org>
2012-11-03 10:18:38 +0100

    added local.d/admin.ini to set admin pw

--- puppet/modules/site_couchdb/manifests/configure.pp
+++ puppet/modules/site_couchdb/manifests/configure.pp
@@ -1,7 +1,18 @@
 class site_couchdb::configure {
-  Class[site_couchdb::package] -> Class[couchdb] 
+  Class[site_couchdb::package] -> Class[couchdb]
+
   class { 'couchdb':
-    require => Class['site_couchdb::package']
-    #bind => '0.0.0.0'
+    require => Class['site_couchdb::package'],
+  }
+
+  $adminpw = hiera('couchdb_adminpw')
+  file { '/etc/couchdb/local.d/admin.ini':
+    content => "[admins]
+admin = $adminpw
+",
+    mode    => '0600',
+    owner   => 'couchdb',
+    group   => 'couchdb',
+    notify  => Service[couchdb]
   }
 }
