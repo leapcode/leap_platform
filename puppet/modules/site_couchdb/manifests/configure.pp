0a3fdfff7bd8c11d6099f23aef505fbd5333ba99
varac <varacanero@zeromail.org>
2012-11-04 20:45:53 +0100

    deploy couchdb initscript, restart couchdb after config file change

--- puppet/modules/site_couchdb/manifests/configure.pp
+++ puppet/modules/site_couchdb/manifests/configure.pp
@@ -2,10 +2,17 @@ class site_couchdb::configure {
   Class[site_couchdb::package] -> Class[couchdb]
 
   class { 'couchdb':
-    require => Class['site_couchdb::package'],
+    require => Class['site_couchdb::package'], }
+
+
+  file { '/etc/init.d/couchdb':
+    source => 'puppet:///modules/site_couchdb/couchdb',
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
   }
 
-  $adminpw = $site_couchdb::adminpw 
+  $adminpw = $site_couchdb::adminpw
   file { '/etc/couchdb/local.d/admin.ini':
     content => "[admins]
 admin = $adminpw
@@ -14,5 +21,13 @@ admin = $adminpw
     owner   => 'couchdb',
     group   => 'couchdb',
     notify  => Service[couchdb]
+  }
+
+
+  exec { '/etc/init.d/couchdb restart; sleep 3':
+    path        => ['/bin', '/usr/bin',],
+    subscribe   => File['/etc/couchdb/local.d/admin.ini',
+      '/etc/couchdb/local.ini'],
+    refreshonly => true
   }
 }
