f3704fc0ac81ca6ccb7e7d19ae931d9c391f3975
elijah <elijah@riseup.net>
2012-11-22 11:43:23 -0800

    clean up openvpn and x509 paths

--- puppet/modules/site_openvpn/manifests/keys.pp
+++ puppet/modules/site_openvpn/manifests/keys.pp
@@ -1,17 +1,12 @@
 class site_openvpn::keys {
 
-  file { '/etc/openvpn/keys/ca.key':
-    content => $site_openvpn::openvpn_config['ca_key'],
-    mode    => '0600',
-  }
-
   file { '/etc/openvpn/keys/ca.crt':
-    content => $site_openvpn::openvpn_config['ca_crt'],
+    content => $site_openvpn::x509_config['ca_cert'],
     mode    => '0644',
   }
 
   file { '/etc/openvpn/keys/dh.pem':
-    content => $site_openvpn::openvpn_config['dh'],
+    content => $site_openvpn::x509_config['dh'],
     mode    => '0644',
   }
 
