8d4e198fc0aa750128230659f6eb68d5a74f0f2a
Micah Anderson <micah@riseup.net>
2012-12-11 16:32:56 -0500

    change hostname exec to only apply when either the /etc/hostname or /etc/hosts files are changed (otherwise it runs on every run)

--- puppet/modules/site_config/manifests/hosts.pp
+++ puppet/modules/site_config/manifests/hosts.pp
@@ -3,13 +3,15 @@ class site_config::hosts() {
   $hosts = hiera('hosts','')
   $hostname = hiera('name')
 
-  exec { "/bin/hostname $hostname": }
-
   file { "/etc/hostname":
     ensure => present,
     content => $hostname
   }
 
+  exec { "/bin/hostname $hostname":
+    subscribe => [ File['/etc/hostname'], File['/etc/hosts'] ]
+  }
+  
   file { '/etc/hosts':
     content => template('site_config/hosts'),
     mode    => '0644', owner => root, group => root;
