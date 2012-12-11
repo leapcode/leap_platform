cbb834da5de7e2abe7399e34766492bfab48fa9c
Micah Anderson <micah@riseup.net>
2012-12-11 15:37:15 -0500

    test to see if the hosts value is empty before trying to reference it in a template
    also set the hostname to what the hiera 'name' is set to

--- puppet/modules/site_config/manifests/hosts.pp
+++ puppet/modules/site_config/manifests/hosts.pp
@@ -1,5 +1,15 @@
 class site_config::hosts {
 
+  $hosts = hiera('hosts','')
+  $hostname = hiera('name')
+
+  exec { "/bin/hostname $hostname ": }
+
+  file { "/etc/hostname":
+    ensure => present,
+    content => $hostname
+  }
+
   file { '/etc/hosts':
     content => template('site_config/hosts'),
     mode    => '0644', owner => root, group => root;
