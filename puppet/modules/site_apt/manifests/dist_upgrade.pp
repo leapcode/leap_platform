27651e6188325880244fe17d3bf82c3068095e8a
varac <varacanero@zeromail.org>
2013-01-18 22:32:47 +0100

    linted

--- puppet/modules/site_apt/manifests/dist_upgrade.pp
+++ puppet/modules/site_apt/manifests/dist_upgrade.pp
@@ -1,11 +1,11 @@
 class site_apt::dist_upgrade inherits apt::dist_upgrade {
 
   # really upgrade on every puppetrun
-  Exec["apt_dist-upgrade"]{
-    refreshonly => false,  
+  Exec['apt_dist-upgrade']{
+    refreshonly => false,
   }
 
   # Ensure apt-get upgrade has been run before installing any packages
   # Disables because apt-get update is moved to stage initial
-  # Exec["apt_dist-upgrade"] -> Package <| name != 'lsb-release' |> 
+  # Exec["apt_dist-upgrade"] -> Package <| name != 'lsb-release' |>
 }
