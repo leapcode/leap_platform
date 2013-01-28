76375b224bd621ab2238fa49207ca928892cd6f5
varac <varacanero@zeromail.org>
2013-01-28 16:59:18 +0100

    disabled notice about nagios services not deployed

--- puppet/modules/site_nagios/manifests/add_service.pp
+++ puppet/modules/site_nagios/manifests/add_service.pp
@@ -1,6 +1,7 @@
 define site_nagios::add_service ($hostname, $ip_address, $service) {
 
   case $service {
+    # don't deploy until we fix 1546
     #'openvpn': {
     #  $check_command       = "check_openvpn_server_ip_port!$ip_address!1194"
     #  $service_description = 'Openvpn'
@@ -9,7 +10,9 @@ define site_nagios::add_service ($hostname, $ip_address, $service) {
       $check_command       = 'check_https'
       $service_description = 'Website'
     }
-  default:  { notice ("No Nagios service check for service \"$service\"") }
+    default:  {
+      #notice ("No Nagios service check for service \"$service\"")
+    }
   }
 
   if ( $check_command != '' ) {
