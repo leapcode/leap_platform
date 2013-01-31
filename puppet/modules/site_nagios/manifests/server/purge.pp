5addc36a364186d53d13304182d6f41b30f6a890
varac <varacanero@zeromail.org>
2013-01-31 11:41:13 +0100

    just purge the nagios3/conf.d content, not the dir itself

--- puppet/modules/site_nagios/manifests/server/purge.pp
+++ puppet/modules/site_nagios/manifests/server/purge.pp
@@ -1,6 +1,6 @@
 class site_nagios::server::purge {
   exec {'purge_conf.d':
-    command => '/bin/rm -rf /etc/nagios3/conf.d',
+    command => '/bin/rm -rf /etc/nagios3/conf.d/*',
     onlyif  => 'test -e /etc/nagios3/conf.d'
   }
 
