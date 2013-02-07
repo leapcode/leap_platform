08720568f7c00373560379e44695b881fff18af1
varac <varacanero@zeromail.org>
2013-02-07 11:48:29 +0100

    working tor relay

--- puppet/modules/site_tor/manifests/exit_policy.pp
+++
@@ -1,8 +0,0 @@
-class site_tor::exit_policy {
-  # exaple policy to allow ssh
-  tor::daemon::exit_policy { 'ssh_exit_policy':
-    accept => '*:22',
-    reject => '*:*';
-  }
-}
-
--- puppet/modules/site_tor/manifests/init.pp
+++ puppet/modules/site_tor/manifests/init.pp
@@ -6,22 +6,22 @@ class site_tor {
   $tor_type = $tor['type']
 
   $contact_email = hiera('contact_email')
+  $address       = hiera('ip_address')
 
   class { 'tor::daemon': }
   tor::daemon::relay { $::hostname:
     port             => 9001,
-    #listen_addresses => '',
+    address          => $address,
     contact_info     => $contact_email,
     bandwidth_rate   => $bandwidth_rate,
   }
 
-  # we configure the directory later
-  #tor::daemon::directory { $::hostname: port => 80 }
+  tor::daemon::directory { $::hostname: port => 80 }
 
   include site_shorewall::tor
 
-  if ( $tor_type == 'exit' ) {
-    include site_tor::exit_policy
+  if ( $tor_type != 'exit' ) {
+    include site_tor::disable_exit
   }
 
 }
