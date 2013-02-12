102af94df02decef888bac09748dbac6773dedd6
varac <varacanero@zeromail.org>
2013-02-12 13:26:42 +0100

    fixed shorewall is blocking api port (Bug #1735)

---
+++ puppet/modules/site_shorewall/manifests/service/webapp_api.pp
@@ -0,0 +1,21 @@
+class site_shorewall::service::webapp_api {
+
+  $api = hiera('api')
+  $api_port = $api['port']
+
+  # define macro for incoming services
+  file { '/etc/shorewall/macro.leap_webapp_api':
+    content => "PARAM   -       -       tcp    $api_port ",
+    notify  => Service['shorewall']
+  }
+
+
+  shorewall::rule {
+      'net2fw-webapp_api':
+        source      => 'net',
+        destination => '$FW',
+        action      => 'leap_webapp_api(ACCEPT)',
+        order       => 200;
+  }
+
+}
--- puppet/modules/site_shorewall/manifests/webapp.pp
+++ puppet/modules/site_shorewall/manifests/webapp.pp
@@ -2,4 +2,5 @@ class site_shorewall::webapp {
 
   include site_shorewall::defaults
   include site_shorewall::service::https
+  include site_shorewall::service::webapp_api
 }
