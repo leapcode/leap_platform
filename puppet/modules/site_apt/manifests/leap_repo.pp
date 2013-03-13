65afb3a013fedd7c2d5eef15ac879a41c51de8e0
varac <varacanero@zeromail.org>
2013-03-13 10:56:24 +0100

    install apt key before apt-get update

--- puppet/modules/site_apt/manifests/leap_repo.pp
+++ puppet/modules/site_apt/manifests/leap_repo.pp
@@ -1,6 +1,7 @@
 class site_apt::leap_repo {
   apt::sources_list {'leap.list':
-    content => "deb http://deb.leap.se/debian $::lsbdistcodename main"
+    content => "deb http://deb.leap.se/debian ${::lsbdistcodename} main",
+    before  => Exec[refresh_apt]
   }
 
 }
