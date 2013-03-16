a275999ab39b49afa2bb0c998c58aec424b4a8c0
varac <varacanero@zeromail.org>
2013-03-16 13:57:14 +0100

    pick the first couchdb host for webapp couch config
    Until we have a proper load balancing setup
    (see https://leap.se/code/issues/1994)

--- puppet/modules/site_webapp/manifests/couchdb.pp
+++ puppet/modules/site_webapp/manifests/couchdb.pp
@@ -1,7 +1,10 @@
 class site_webapp::couchdb {
 
   $webapp           = hiera('webapp')
-  $couchdb_host     = $webapp['couchdb_hosts']
+  $couchdb_hosts    = $webapp['couchdb_hosts']
+  # for now, pick the first couchdb host before we have a working
+  # load balancing setup (see https://leap.se/code/issues/1994)
+  $couchdb_host     = $couchdb_hosts[0]
   $couchdb_user     = $webapp['couchdb_user']['username']
   $couchdb_password = $webapp['couchdb_user']['password']
 
