9c1c74c359f80cf0e61b62befee0ec5cc04ab4c3
Micah Anderson <micah@riseup.net>
2013-03-19 17:41:37 -0400

    create a separate couchdb.yml.admin that contains the couchdb admin privileges, putting the unprivileged ones in as user webapp in couchdb.yml. This allows us to migrate the couchdb design docs on deployment, but use an unprivileged user the remainder of the time

--- puppet/modules/site_webapp/manifests/couchdb.pp
+++ puppet/modules/site_webapp/manifests/couchdb.pp
@@ -1,19 +1,27 @@
 class site_webapp::couchdb {
 
-  $x509             = hiera('x509')
-  $key              = $x509['key']
-  $cert             = $x509['cert']
-  $ca               = $x509['ca_cert']
-  $webapp           = hiera('webapp')
-  $couchdb_hosts    = $webapp['couchdb_hosts']
+  $x509                    = hiera('x509')
+  $key                     = $x509['key']
+  $cert                    = $x509['cert']
+  $ca                      = $x509['ca_cert']
+  $webapp                  = hiera('webapp')
+  $couchdb_hosts           = $webapp['couchdb_hosts']
   # haproxy listener on port localhost:4096, see site_webapp::haproxy
-  $couchdb_host     = 'localhost'
-  $couchdb_port     = '4096'
-  $couchdb_user     = $webapp['couchdb_user']['username']
-  $couchdb_password = $webapp['couchdb_user']['password']
+  $couchdb_host            = 'localhost'
+  $couchdb_port            = '4096'
+  $couchdb_admin_user      = $webapp['couchdb_admin_user']['username']
+  $couchdb_admin_password  = $webapp['couchdb_admin_user']['password']
+  $couchdb_webapp_user     = $webapp['couchdb_webapp_user']['username']
+  $couchdb_webapp_password = $webapp['couchdb_webapp_user']['password']
 
   file {
-    '/srv/leap-webapp/config/couchdb.yml':
+    '/srv/leap-webapp/config/couchdb.yml.admin':
+      content => template('site_webapp/couchdb.yml.admin.erb'),
+      owner   => leap-webapp,
+      group   => leap-webapp,
+      mode    => '0600';
+
+    '/srv/leap-webapp/config/couchdb.yml.webapp':
       content => template('site_webapp/couchdb.yml.erb'),
       owner   => leap-webapp,
       group   => leap-webapp,
