eac4d82da1675d839fcdc2360df5929e41322c2d
varac <varacanero@zeromail.org>
2013-03-21 14:03:07 +0100

    start erlang vm on dedicated port so firewalling is easier

--- puppet/modules/site_shorewall/manifests/couchdb.pp
+++ puppet/modules/site_shorewall/manifests/couchdb.pp
@@ -7,9 +7,12 @@ class site_shorewall::couchdb {
   # bigcouch cluster nodes
   $portmapper_port = '5369'
 
+  # see http://stackoverflow.com/questions/8459949/bigcouch-cluster-connection-issue#comment10467603_8463814
+  $erlang_vm_port = '9001'
+
   # define macro for incoming services
   file { '/etc/shorewall/macro.leap_couchdb':
-    content => "PARAM   -       -       tcp    ${couchdb_port},${portmapper_port}",
+    content => "PARAM   -       -       tcp    ${couchdb_port},${portmapper_port},${erlang_vm_port}",
     notify  => Service['shorewall'],
     require => Package['shorewall']
   }
