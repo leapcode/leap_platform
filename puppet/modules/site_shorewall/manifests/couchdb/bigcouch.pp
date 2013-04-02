0227e03513f38cfae4a041ba6740b425fdc21198
Micah Anderson <micah@riseup.net>
2013-04-02 17:09:22 -0400

    replace hard-coded port number with hiera determined one, manipulated to remove the 'ip:' from the beginning in bigcouch replication client stunnels

--- puppet/modules/site_shorewall/manifests/couchdb/bigcouch.pp
+++ puppet/modules/site_shorewall/manifests/couchdb/bigcouch.pp
@@ -7,6 +7,7 @@ class site_shorewall::couchdb::bigcouch {
 
   $bigcouch_replication_server          = $stunnel['bigcouch_replication_server']
   $bigcouch_replication_server_port     = $bigcouch_replication_server['accept']
+  $bigcouch_replication_connect         = $bigcouch_replication_server['connect']
 
   # define macro for incoming services
   file { '/etc/shorewall/macro.leap_bigcouch':
@@ -26,7 +27,7 @@ class site_shorewall::couchdb::bigcouch {
   $bigcouch_shorewall_dnat_defaults = {
     'source'          => '$FW',
     'proto'           => 'tcp',
-    'destinationport' => '4369',
+    'destinationport' => regsubst($bigcouch_replication_connect, '^([0-9.]+:)([0-9]+)$', '\2')
   }
 
   create_resources(site_shorewall::couchdb::dnat, $bigcouch_replication_clients, $bigcouch_shorewall_dnat_defaults)
