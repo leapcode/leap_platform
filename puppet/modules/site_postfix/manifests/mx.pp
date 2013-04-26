a7bcd40d22769ec61b3fdb8d04dd447f6167ed7e
varac <varacanero@zeromail.org>
2013-04-26 10:09:21 +0200

    using alias resolver

--- puppet/modules/site_postfix/manifests/mx.pp
+++ puppet/modules/site_postfix/manifests/mx.pp
@@ -11,28 +11,15 @@ class site_postfix::mx {
     'mydestination':
       value => "\$myorigin, localhost, localhost.\$mydomain, ${domain}";
     'smtpd_recipient_restrictions':
-      value => 'check_recipient_access hash:/etc/postfix/recipient,reject_unauth_destination';
-    'mailbox_size_limit':
-      value => '0';
-    'home_mailbox':
-      value => 'Maildir/';
-    'virtual_alias_maps':
-      value => 'hash:/etc/postfix/virtual';
+      value => 'check_recipient_access tcp:localhost:2244,reject_unauth_destination';
+    'mailbox_size_limit':   value => '0';
+    'home_mailbox':         value => 'Maildir/';
+    'virtual_alias_maps':   value => 'tcp:localhost:4242';
+    'luser_relay':          value => 'vmail';
+    'local_recipient_maps': value => '';
   }
 
-  postfix::hash { '/etc/postfix/virtual': }
-  postfix::hash { '/etc/postfix/recipient': }
-
-  # for now, accept all mail
-  line {'deliver to vmail':
-    file    => '/etc/postfix/recipient',
-    line    => "@${domain} vmail",
-    notify  => Exec['generate /etc/postfix/recipient.db'],
-    require => Package['postfix'],
-  }
-
-  postfix::virtual { "@${domain}": destination => 'vmail'; }
-  #postfix::mailalias { 'vmail': recipient => 'vmail' }
+  #include site_postfix::mx::smtp_checks
 
   user { 'vmail':
     ensure     => present,
