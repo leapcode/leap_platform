8087a374429c1b1be5a766e6e89cdeb02de292a3
elijah <elijah@riseup.net>
2013-05-02 14:01:35 -0700

    fixed dnat_rules

--- puppet/modules/site_shorewall/manifests/dnat_rule.pp
+++ puppet/modules/site_shorewall/manifests/dnat_rule.pp
@@ -2,12 +2,12 @@ define site_shorewall::dnat_rule {
 
   $port = $name
   if $port != 1194 {
-    if $site_openvpn::openvpn_allow_paid {
+    if $site_openvpn::openvpn_allow_unlimited {
       shorewall::rule {
           "dnat_tcp_port_$port":
             action          => 'DNAT',
             source          => 'net',
-            destination     => "\$FW:${site_openvpn::paid_gateway_address}:1194",
+            destination     => "\$FW:${site_openvpn::unlimited_gateway_address}:1194",
             proto           => 'tcp',
             destinationport => $port,
             order           => 100;
@@ -16,18 +16,18 @@ define site_shorewall::dnat_rule {
           "dnat_udp_port_$port":
             action          => 'DNAT',
             source          => 'net',
-            destination     => "\$FW:${site_openvpn::paid_gateway_address}:1194",
+            destination     => "\$FW:${site_openvpn::unlimited_gateway_address}:1194",
             proto           => 'udp',
             destinationport => $port,
             order           => 100;
       }
     }
-    if $site_openvpn::openvpn_allow_free {
+    if $site_openvpn::openvpn_allow_limited {
       shorewall::rule {
           "dnat_free_tcp_port_$port":
             action          => 'DNAT',
             source          => 'net',
-            destination     => "\$FW:${site_openvpn::free_gateway_address}:1194",
+            destination     => "\$FW:${site_openvpn::limited_gateway_address}:1194",
             proto           => 'tcp',
             destinationport => $port,
             order           => 100;
@@ -36,7 +36,7 @@ define site_shorewall::dnat_rule {
           "dnat_free_udp_port_$port":
             action          => 'DNAT',
             source          => 'net',
-            destination     => "\$FW:${site_openvpn::free_gateway_address}:1194",
+            destination     => "\$FW:${site_openvpn::limited_gateway_address}:1194",
             proto           => 'udp',
             destinationport => $port,
             order           => 100;
