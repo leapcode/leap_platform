4132a1b857c79670ef457ae7e11b77b6ff2c477b
Micah Anderson <micah@riseup.net>
2013-05-09 17:16:48 -0400

    the leap platform repo should track 'stable'

--- puppet/modules/site_apt/manifests/leap_repo.pp
+++ puppet/modules/site_apt/manifests/leap_repo.pp
@@ -1,6 +1,6 @@
 class site_apt::leap_repo {
-  apt::sources_list {'leap.list':
-    content => "deb http://deb.leap.se/debian ${::lsbdistcodename} main",
+  apt::sources_list { 'leap.list':
+    content => 'deb http://deb.leap.se/debian stable main',
     before  => Exec[refresh_apt]
   }
 
