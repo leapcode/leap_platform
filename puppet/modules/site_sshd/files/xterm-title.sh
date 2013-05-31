c92d3ac0780e813a5440c5e475bfdba5de5a0447
elijah <elijah@riseup.net>
2013-05-30 17:06:14 -0700

    site_sshd -- added xterm title, optional support for mosh

---
+++ puppet/modules/site_sshd/files/xterm-title.sh
@@ -0,0 +1,8 @@
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD}\007"'
+    ;;
+*)
+    ;;
+esac
--- puppet/modules/site_sshd/manifests/init.pp
+++ puppet/modules/site_sshd/manifests/init.pp
@@ -1,1 +1,41 @@
-class site_sshd {}
+class site_sshd {
+  $ssh = hiera_hash('ssh')
+
+  ##
+  ## XTERM TITLE
+  ##
+
+  file {'/etc/profile.d/xterm-title.sh':
+    source => "puppet://$server/modules/site_sshd/xterm-title.sh",
+    owner => root, group => 0, mode => 0644;
+  }
+
+  ##
+  ## OPTIONAL MOSH SUPPORT
+  ##
+
+  $mosh = $ssh['mosh']
+  $mosh_ports = $mosh['ports']
+  if $ssh['mosh']['enabled'] {
+    $mosh_ensure = present
+  } else {
+    $mosh_ensure = absent
+  }
+
+  package { 'mosh':
+    ensure => $mosh_ensure;
+  }
+  file { '/etc/shorewall/macro.mosh':
+    ensure  => $mosh_ensure,
+    content => "PARAM   -       -       udp    $mosh_ports",
+    notify  => Service['shorewall'],
+    require => Package['shorewall'];
+  }
+  shorewall::rule { 'net2fw-mosh':
+    ensure      => $mosh_ensure,
+    source      => 'net',
+    destination => '$FW',
+    action      => 'mosh(ACCEPT)',
+    order       => 200;
+  }
+}
