8579e89110c3df333ec3bb63154bba2c57a848a9
varac <varacanero@zeromail.org>
2013-06-25 18:12:46 +0200

    include global variable for apt config, include Exec[] defaults

--- puppet/manifests/setup.pp
+++ puppet/manifests/setup.pp
@@ -2,6 +2,10 @@
 # this is applied before each run of site.pp
 #
 $services = ''
+
+Exec { path => '/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin' }
+$custom_key_dir = 'puppet:///modules/site_apt/keys'
+
 include site_config::hosts
 
 include site_apt
