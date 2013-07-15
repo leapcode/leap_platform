f64791335e40b2b6e05305a6d8dda989fb755b9d
varac <varacanero@zeromail.org>
2013-07-15 11:34:22 +0200

    fix smtpd_recipient_restrictions, debug connections from localhost

--- puppet/modules/site_postfix/manifests/mx.pp
+++ puppet/modules/site_postfix/manifests/mx.pp
@@ -12,13 +12,13 @@ class site_postfix::mx {
     'mydestination':
       value => "\$myorigin, localhost, localhost.\$mydomain, ${domain}";
     'smtpd_recipient_restrictions':
-      value => 'check_recipient_access tcp:localhost:2244,reject_unauth_destination,permit_tls_all_clientcerts';
+      value => 'check_recipient_access tcp:localhost:2244,permit_tls_all_clientcerts,reject_unauth_destination';
     'mailbox_size_limit':   value  => '0';
     'home_mailbox':         value  => 'Maildir/';
     'virtual_alias_maps':   value  => 'tcp:localhost:4242';
     'luser_relay':          value  => 'vmail';
     'local_recipient_maps': value  => '';
-    #'debug_peer_list':      value => '127.0.0.1';
+    'debug_peer_list':      value => '127.0.0.1';
   }
 
   include site_postfix::mx::smtpd_checks
