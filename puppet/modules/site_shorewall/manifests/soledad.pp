da191971398827f81ddb0dffd86d4a3c572f6386
Micah Anderson <micah@leap.se>
2013-07-29 21:00:21 -0400

    make site_shorewall::soledad use the hiera value for the soledad port
    Change-Id: I923f15de807f907d6246c3a83df1e59c39d4e920

--- puppet/modules/site_shorewall/manifests/soledad.pp
+++ puppet/modules/site_shorewall/manifests/soledad.pp
@@ -1,10 +1,13 @@
 class site_shorewall::soledad {
 
+  $soledad      = hiera('soledad')
+  $soledad_port = $soledad['port']
+
   include site_shorewall::defaults
 
   # define macro for incoming services
   file { '/etc/shorewall/macro.leap_soledad':
-    content => 'PARAM   -       -       tcp    2424',
+    content => "PARAM   -       -       tcp    ${soledad_port}",
     notify  => Service['shorewall'],
     require => Package['shorewall']
   }
