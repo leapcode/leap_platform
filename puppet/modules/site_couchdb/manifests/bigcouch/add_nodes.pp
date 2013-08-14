9aa06a43fdc2a4eee50556e46b40d5003eb25925
Micah Anderson <micah@leap.se>
2013-08-13 22:21:44 -0400

    require that the couchdb::query::setup has been run before any attempts are made to create databases or add users as these would fail otherwise. Closes: #3466
    Change-Id: Ifa8b3da5858ce858fd319c4a659e70d20a65d3e0

--- puppet/modules/site_couchdb/manifests/bigcouch/add_nodes.pp
+++ puppet/modules/site_couchdb/manifests/bigcouch/add_nodes.pp
@@ -1,5 +1,8 @@
 class site_couchdb::bigcouch::add_nodes {
   # loop through neighbors array and add nodes
   $nodes = $::site_couchdb::bigcouch_config['neighbors']
-  couchdb::bigcouch::add_node { $nodes: }
+
+  couchdb::bigcouch::add_node { $nodes:
+    require => Couchdb::Query::Setup['localhost']
+  }
 }
