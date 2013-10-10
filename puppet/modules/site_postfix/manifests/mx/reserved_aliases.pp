b22a3524c99f9205c8078d5542718e3625e68244
varac <varacanero@zeromail.org>
2013-10-10 18:14:24 +0200

    fix site_postfix::mx::reserved_aliases class name and package array

--- puppet/modules/site_postfix/manifests/mx/reserved_aliases.pp
+++ puppet/modules/site_postfix/manifests/mx/reserved_aliases.pp
@@ -1,11 +1,11 @@
-class site_postfix::reserved_aliases {
+class site_postfix::mx::reserved_aliases {
 
   postfix::mailalias {
     [ 'postmaster', 'hostmaster', 'domainadmin', 'certmaster', 'ssladmin',
       'arin-admin', 'administrator', 'webmaster', 'www-data', 'www',
       'nobody', 'sys', 'postgresql', 'mysql', 'bin', 'cron', 'lp', 'games',
       'maildrop', 'abuse', 'noc', 'security', 'usenet', 'news', 'uucp',
-      'ftp':
+      'ftp' ]:
       ensure    => present,
       recipient => 'root'
   }
