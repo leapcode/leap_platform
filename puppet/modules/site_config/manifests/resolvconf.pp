befbc71fa2af217ff89facd8e10794b60f19f66e
varac <varacanero@zeromail.org>
2013-10-15 22:43:49 +0200

    new fallback nameservers (#4113)
    * the german privacy foundation has dissolved itself and shut down their
      public nameserver. we are now using the public nameserver by Digitalcourage,
      a german privacy organisation (https://en.wikipedia.org/wiki/Digitalcourage)
    * the IP for the server of the swiss privacy foundation has changed
      (http://www.privacyfoundation.ch/de/service/server.html)

--- puppet/modules/site_config/manifests/resolvconf.pp
+++ puppet/modules/site_config/manifests/resolvconf.pp
@@ -2,12 +2,13 @@ class site_config::resolvconf {
 
   $domain_public = $site_config::default::domain_hash['full_suffix']
 
-  # 127.0.0.1:      caching-only local bind
-  # 87.118.100.175: http://server.privacyfoundation.de
-  # 62.141.58.13:   http://www.privacyfoundation.ch/de/service/server.html
   class { '::resolvconf':
     domain      => $domain_public,
     search      => $domain_public,
-    nameservers => [ '127.0.0.1', '87.118.100.175', '62.141.58.13' ]
+    nameservers => [
+      '127.0.0.1      # local caching-only, unbound',
+      '85.214.20.141  # Digitalcourage, a german privacy organisation: (https://en.wikipedia.org/wiki/Digitalcourage)',
+      '62.141.58.13   # Swiss privacy Foundation (http://www.privacyfoundation.ch/de/service/server.html)'
+    ]
   }
 }
