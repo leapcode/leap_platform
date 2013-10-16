a44e598c29b66bd560dbe864b70f13aa324c1437
Micah Anderson <micah@leap.se>
2013-10-16 15:59:26 -0400

    fix for rsyslog-relp being installed first, resulting in dependency errors (#4161)
    Change-Id: I2f0bcc5b4cb5effae57051f04251aeb8b09a4c6d

--- puppet/modules/site_config/manifests/syslog.pp
+++ puppet/modules/site_config/manifests/syslog.pp
@@ -3,14 +3,14 @@ class site_config::syslog {
   # we need to pull in rsyslog from the leap repository until it is availbale in
   # wheezy-backports
   apt::preferences_snippet { 'fixed_rsyslog_anon_package':
-    package  => 'rsyslog',
+    package  => 'rsyslog-*',
     priority => '999',
     pin      => 'release o=leap.se',
     before   => Class['rsyslog::install']
   }
 
-  apt::preferences_snippet { 'rsyslog_anon_libestr0':
-    package  => 'libestr0',
+  apt::preferences_snippet { 'rsyslog_anon_depends':
+    package  => 'libestr0 librelp0',
     priority => '999',
     pin      => 'release a=wheezy-backports',
     before   => Class['rsyslog::install']
