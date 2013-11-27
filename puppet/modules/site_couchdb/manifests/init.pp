27b084fbb61ea036a45970f1b0c5a8f8175f96e8
Micah Anderson <micah@leap.se>
2013-11-27 15:00:10 -0500

    add missing nickserver hiera value lookup
    Change-Id: I77061054f4768f0677ca9c498e6cd6d5df4ff806

--- puppet/modules/site_couchdb/manifests/init.pp
+++ puppet/modules/site_couchdb/manifests/init.pp
@@ -1,40 +1,45 @@
 class site_couchdb {
   tag 'leap_service'
 
-  $couchdb_config         = hiera('couch')
-  $couchdb_users          = $couchdb_config['users']
+  $couchdb_config          = hiera('couch')
+  $couchdb_users           = $couchdb_config['users']
 
-  $couchdb_admin          = $couchdb_users['admin']
-  $couchdb_admin_user     = $couchdb_admin['username']
-  $couchdb_admin_pw       = $couchdb_admin['password']
-  $couchdb_admin_salt     = $couchdb_admin['salt']
+  $couchdb_admin           = $couchdb_users['admin']
+  $couchdb_admin_user      = $couchdb_admin['username']
+  $couchdb_admin_pw        = $couchdb_admin['password']
+  $couchdb_admin_salt      = $couchdb_admin['salt']
 
-  $couchdb_leap_mx        = $couchdb_users['leap_mx']
-  $couchdb_leap_mx_user   = $couchdb_leap_mx['username']
-  $couchdb_leap_mx_pw     = $couchdb_leap_mx['password']
-  $couchdb_leap_mx_salt   = $couchdb_leap_mx['salt']
+  $couchdb_leap_mx         = $couchdb_users['leap_mx']
+  $couchdb_leap_mx_user    = $couchdb_leap_mx['username']
+  $couchdb_leap_mx_pw      = $couchdb_leap_mx['password']
+  $couchdb_leap_mx_salt    = $couchdb_leap_mx['salt']
 
-  $couchdb_soledad        = $couchdb_users['soledad']
-  $couchdb_soledad_user   = $couchdb_soledad['username']
-  $couchdb_soledad_pw     = $couchdb_soledad['password']
-  $couchdb_soledad_salt   = $couchdb_soledad['salt']
+  $couchdb_nickserver      = $couchdb_users['nickserver']
+  $couchdb_nickserver_user = $couchdb_nickserver['username']
+  $couchdb_nickserver_pw   = $couchdb_nickserver['password']
+  $couchdb_nickserver_salt = $couchdb_nickserver['salt']
+
+  $couchdb_soledad         = $couchdb_users['soledad']
+  $couchdb_soledad_user    = $couchdb_soledad['username']
+  $couchdb_soledad_pw      = $couchdb_soledad['password']
+  $couchdb_soledad_salt    = $couchdb_soledad['salt']
 
   $couchdb_tapicero        = $couchdb_users['tapicero']
   $couchdb_tapicero_user   = $couchdb_tapicero['username']
   $couchdb_tapicero_pw     = $couchdb_tapicero['password']
   $couchdb_tapicero_salt   = $couchdb_tapicero['salt']
 
-  $couchdb_webapp         = $couchdb_users['webapp']
-  $couchdb_webapp_user    = $couchdb_webapp['username']
-  $couchdb_webapp_pw      = $couchdb_webapp['password']
-  $couchdb_webapp_salt    = $couchdb_webapp['salt']
+  $couchdb_webapp          = $couchdb_users['webapp']
+  $couchdb_webapp_user     = $couchdb_webapp['username']
+  $couchdb_webapp_pw       = $couchdb_webapp['password']
+  $couchdb_webapp_salt     = $couchdb_webapp['salt']
 
-  $couchdb_backup         = $couchdb_config['backup']
+  $couchdb_backup          = $couchdb_config['backup']
 
-  $bigcouch_config        = $couchdb_config['bigcouch']
-  $bigcouch_cookie        = $bigcouch_config['cookie']
+  $bigcouch_config         = $couchdb_config['bigcouch']
+  $bigcouch_cookie         = $bigcouch_config['cookie']
 
-  $ednp_port              = $bigcouch_config['ednp_port']
+  $ednp_port               = $bigcouch_config['ednp_port']
 
   class { 'couchdb':
     bigcouch        => true,
