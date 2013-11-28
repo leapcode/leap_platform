81ff8e2b6aaebae35098357146958a8011af85fe
Micah Anderson <micah@leap.se>
2013-11-28 11:56:38 -0500

    fix cwd in webapp cronjobs
    Change-Id: I2e189bf5e209c731077539a57f9b334ac66cd25b

--- puppet/modules/site_webapp/manifests/cron.pp
+++ puppet/modules/site_webapp/manifests/cron.pp
@@ -3,15 +3,13 @@ class site_webapp::cron {
   # cron tasks that need to be performed to cleanup the database
   cron {
     'remove_expired_sessions':
-      command     => 'bundle exec rake cleanup:sessions',
-      cwd         => '/srv/leap/webapp',
+      command     => 'cd /srv/leap/webapp && bundle exec rake cleanup:sessions',
       environment => 'RAILS_ENV=production',
       hour        => 2,
       minute      => 30;
 
     'remove_expired_tokens':
-      command     => 'bundle exec rake cleanup:tokens',
-      cwd         => '/srv/leap/webapp',
+      command     => 'cd /srv/leap/webapp && bundle exec rake cleanup:tokens',
       environment => 'RAILS_ENV=production',
       hour        => 3,
       minute      => 0;
