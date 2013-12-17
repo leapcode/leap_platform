9531b13447ff204a00a138a137818054603fe1c9
Micah Anderson <micah@leap.se>
2013-12-17 12:58:22 -0500

    add a smtp_tls class and include that on both mx servers and satellites
    Change-Id: I779ea60e6d726d042203fa0756d73b4af079d728

--- puppet/modules/site_postfix/manifests/satellite.pp
+++ puppet/modules/site_postfix/manifests/satellite.pp
@@ -3,9 +3,12 @@ class site_postfix::satellite {
   $root_mail_recipient = hiera ('contacts')
   $mail                = hiera ('mail')
   $relayhost           = $mail['smarthost']
+  $cert_name           = hiera('name')
 
   class { '::postfix::satellite':
     relayhost           => $relayhost,
     root_mail_recipient => $root_mail_recipient
   }
+
+  include site_postfix::mx::smtp_tls
 }
