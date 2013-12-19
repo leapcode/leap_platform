e0c07465d14d701da16e47b9c2e3938c80088c4d
varac <varacanero@zeromail.org>
2013-12-19 12:50:17 +0100

    Deploy /etc/leap/couchdb_scripts_defaults.conf so  we can exclude some DBs, i.e. sessions + tokens (#4794)

--- puppet/modules/site_couchdb/manifests/backup.pp
+++ puppet/modules/site_couchdb/manifests/backup.pp
@@ -9,4 +9,15 @@ class site_couchdb::backup {
   backupninja::sh { 'couchdb_backup':
     command_string => "cd /srv/leap/couchdb/scripts \n./couchdb_dumpall.sh"
   }
+
+  # Deploy /etc/leap/couchdb_scripts_defaults.conf so we can exclude
+  # some databases
+
+  file { '/etc/leap/couchdb_scripts_defaults.conf':
+    source => 'puppet:///modules/site_couchdb/couchdb_scripts_defaults.conf',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+
 }
