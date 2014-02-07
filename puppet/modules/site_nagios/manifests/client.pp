d400d271e616f669cc6383a5893dd992a0efada2
varac <varacanero@zeromail.org>
2014-02-07 15:42:12 +0100

    deploy check_mk pubkey on clients

--- puppet/modules/site_nagios/manifests/client.pp
+++ puppet/modules/site_nagios/manifests/client.pp
@@ -1,4 +1,9 @@
 class site_nagios::client {
+
+  $ssh_hash = hiera('ssh')
+  $pubkey   = $ssh_hash['authorized_keys']['monitor']['key']
+  $type     = $ssh_hash['authorized_keys']['monitor']['type']
+
   class { 'check_mk::agent':
     agent_package_name          => 'check-mk-agent',
     agent_logwatch_package_name => 'check-mk-agent-logwatch',
@@ -6,4 +11,12 @@ class site_nagios::client {
     homedir                     => '/etc/nagios/check_mk',
     register_agent              => false
   }
+
+  file { '/root/.ssh/authorized_keys2':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => "command=\"/usr/bin/check_mk_agent\",no-port-forwarding,no-x11-forwarding,no-agent-forwarding ${type} ${pubkey} monitor"
+  }
+
 }
