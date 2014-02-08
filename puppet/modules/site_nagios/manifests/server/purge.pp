6720bdba0c67893de713eee7f753fb582d06aa61
varac <varacanero@zeromail.org>
2014-02-08 14:00:10 +0100

    disable purging of /etc/nagios3 and /etc/nagios3/conf.d

--- puppet/modules/site_nagios/manifests/server/purge.pp
+++ puppet/modules/site_nagios/manifests/server/purge.pp
@@ -1,7 +1,17 @@
-class site_nagios::server::purge {
-  exec {'purge_conf.d':
-    command => '/bin/rm -rf /etc/nagios3/conf.d/*',
-    onlyif  => 'test -e /etc/nagios3/conf.d'
+class site_nagios::server::purge inherits nagios::base {
+  # we don't want to get /etc/nagios3 and /etc/nagios3/conf.d
+  # purged, cause the check-mk-config-nagios3 package
+  # places its templates in /etc/nagios3/conf.d/check_mk,
+  # and check_mk -O updated it's nagios config in /etc/nagios3/conf.d/check_mk
+  File['nagios_cfgdir'] {
+    purge => false
+  }
+  File['nagios_confd'] {
+    purge => false
   }
 
+  exec {'purge_conf.d':
+    command => '/bin/rm -f /etc/nagios3/conf.d/nagios_*',
+    onlyif  => 'find /etc/nagios3/conf.d/ | grep -q "/etc/nagios3/conf.d/nagios_"'
+  }
 }
