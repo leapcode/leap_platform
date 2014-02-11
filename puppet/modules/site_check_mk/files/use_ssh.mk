27cd293b4ed3f3632044e914375a45a5345e8cee
varac <varacanero@zeromail.org>
2014-02-11 14:00:45 +0100

    use <HOST> instead of <IP>, because we cannot use public ips in DC

--- puppet/modules/site_check_mk/files/use_ssh.mk
+++ puppet/modules/site_check_mk/files/use_ssh.mk
@@ -1,5 +1,5 @@
 # http://mathias-kettner.de/checkmk_datasource_programs.html
 datasource_programs = [
- ( "ssh -l root -i /etc/check_mk/.ssh/id_rsa <IP> check_mk_agent", ALL_HOSTS ),
+ ( "ssh -l root -i /etc/check_mk/.ssh/id_rsa <HOST> check_mk_agent", ALL_HOSTS ),
 ]
 
