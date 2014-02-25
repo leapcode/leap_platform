59b09ce45333778619baca2bd4e34e5beba63584
varac <varacanero@zeromail.org>
2014-02-25 13:55:15 +0100

    we use logwatch now to check for bigcouch errors

--- puppet/modules/site_check_mk/manifests/agent/couchdb.pp
+++ puppet/modules/site_check_mk/manifests/agent/couchdb.pp
@@ -3,13 +3,4 @@ class site_check_mk::agent::couchdb {
   file { '/etc/check_mk/logwatch.d/couchdb.cfg':
     source => 'puppet:///modules/site_check_mk/agent/logwatch/couchdb.cfg',
   }
-
-
-  # local custom checks
-  file { '/usr/lib/check_mk_agent/local/check_bigcouch_errors.sh':
-    ensure  => link,
-    target  => '/srv/leap/couchdb/scripts/tests/check_bigcouch_errors.sh',
-    require => Vcsrepo['/srv/leap/couchdb/scripts']
-  }
-
 }
