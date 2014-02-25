91365fb3b9b455253484fca6ffbc3dac8361e5b9
varac <varacanero@zeromail.org>
2014-02-25 15:11:56 +0100

    check syslog for /usr/local/bin/couch-doc-update failures

--- puppet/modules/site_check_mk/manifests/agent/couchdb.pp
+++ puppet/modules/site_check_mk/manifests/agent/couchdb.pp
@@ -4,4 +4,10 @@ class site_check_mk::agent::couchdb {
     source => 'puppet:///modules/site_check_mk/agent/logwatch/bigcouch.cfg',
   }
 
+  concat::fragment { 'syslog_couchdb':
+    source  => 'puppet:///modules/site_check_mk/agent/logwatch/syslog/couchdb.cfg',
+    target  => '/etc/check_mk/logwatch.d/syslog.cfg',
+    order   => '02';
+  }
+
 }
