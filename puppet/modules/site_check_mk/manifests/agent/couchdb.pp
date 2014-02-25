0a6fbced6f2177464c000fc79a90c03171a3bcf6
varac <varacanero@zeromail.org>
2014-02-25 16:45:33 +0100

    complete nagios monitoring for couchdb (Feature #2175)

--- puppet/modules/site_check_mk/manifests/agent/couchdb.pp
+++ puppet/modules/site_check_mk/manifests/agent/couchdb.pp
@@ -1,13 +1,25 @@
 class site_check_mk::agent::couchdb {
 
+  # watch logs
   file { '/etc/check_mk/logwatch.d/bigcouch.cfg':
     source => 'puppet:///modules/site_check_mk/agent/logwatch/bigcouch.cfg',
   }
-
   concat::fragment { 'syslog_couchdb':
     source  => 'puppet:///modules/site_check_mk/agent/logwatch/syslog/couchdb.cfg',
     target  => '/etc/check_mk/logwatch.d/syslog.cfg',
     order   => '02';
   }
+
+
+  # check bigcouch processes
+  file_line {
+    'Bigcouch_epmd_procs':
+      line => 'Bigcouch_epmd_procs  /usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -a /opt/bigcouch/erts-5.9.1/bin/epmd',
+      path => '/etc/check_mk/mrpe.cfg';
+    'Bigcouch_beam_procs':
+      line => 'Bigcouch_beam_procs  /usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -a /opt/bigcouch/erts-5.9.1/bin/beam',
+      path => '/etc/check_mk/mrpe.cfg';
+  }
+
 
 }
