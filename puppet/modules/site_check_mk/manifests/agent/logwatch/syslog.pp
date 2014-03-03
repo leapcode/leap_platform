984939c4a74b71ce67db27a035ca5017f82c416f
varac <varacanero@zeromail.org>
2014-03-03 17:32:42 +0100

    move generic syslog patterns to the end of syslog.cfg so we can ignore patterns first

--- puppet/modules/site_check_mk/manifests/agent/logwatch/syslog.pp
+++ puppet/modules/site_check_mk/manifests/agent/logwatch/syslog.pp
@@ -5,9 +5,14 @@ class site_check_mk::agent::logwatch::syslog {
   }
 
   concat::fragment { 'syslog_header':
-    source => 'puppet:///modules/site_check_mk/agent/logwatch/syslog.cfg',
+    source => 'puppet:///modules/site_check_mk/agent/logwatch/syslog_header.cfg',
     target => '/etc/check_mk/logwatch.d/syslog.cfg',
     order  => '01';
+  }
+  concat::fragment { 'syslog_tail':
+    source => 'puppet:///modules/site_check_mk/agent/logwatch/syslog_tail.cfg',
+    target => '/etc/check_mk/logwatch.d/syslog.cfg',
+    order  => '99';
   }
 
 }
