54fcafe131c411a49e4277cd0d14c6ea20044203
irregulator <irregulator@riseup.net>
2014-05-20 23:20:58 +0300

    Initial commit for obfsproxy server feature in platform

---
+++ puppet/modules/site_obfsproxy/manifests/init.pp
@@ -0,0 +1,28 @@
+class site_obfsproxy {
+  tag 'leap_service'
+  Class['site_config::default'] -> Class['site_obfsproxy']
+
+  $transport = 'scramblesuit'
+
+  $obfsproxy = hiera('obfsproxy')
+  $scramblesuit = $obfsproxy['scramblesuit']
+  $scram_pass = $scramblesuit['password']
+  $scram_port = $scramblesuit['port']
+  $dest_ip   = $obfsproxy['gateway_address']
+  $dest_port = '443'
+
+  include site_apt::preferences::twisted
+  include site_apt::preferences::obfsproxy
+
+  class { 'obfsproxy':
+    transport => $transport,
+    port      => $scram_port,
+    param     => $scram_pass,
+    dest_ip   => $dest_ip,
+    dest_port => $dest_port,
+  }
+
+}
+
+
+
