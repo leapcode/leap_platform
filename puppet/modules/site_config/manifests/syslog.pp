512cd3900b742e01a4a205d2c504741aab5fe84d
elijah <elijah@riseup.net>
2015-02-09 22:29:58 -0800

    logs deploy information to /var/log/leap/deploy.log (full puppet log) and /var/log/leap/deploy-summary.log (just the start and complete, with platform version, user, leap_cli version, platform branch). downgrading platform require --downgrade (requires new leap_cli)

--- puppet/modules/site_config/manifests/syslog.pp
+++ puppet/modules/site_config/manifests/syslog.pp
@@ -11,4 +11,13 @@ class site_config::syslog {
     content => '$ModLoad mmanon
 action(type="mmanon" ipv4.bits="32" mode="rewrite")'
   }
+
+  augeas {
+    'logrotate_leap_deploy':
+      context => '/files/etc/logrotate.d/leap_deploy/rule',
+      changes => [ 'set file /var/log/leap/deploy*.log', 'set rotate 7',
+                   'set schedule daily', 'set compress compress',
+                   'set missingok missingok',
+                   'set copytruncate copytruncate' ]
+  }
 }
