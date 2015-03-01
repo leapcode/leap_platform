6019eff30ef839e5c6a603fde7fe3fc98d4d4f47
varac <varacanero@zeromail.org>
2015-03-01 10:16:44 +0100

    added local check_mk couchdb script (#6749)
    leap_couch_stats.sh is a local check_mk agent script
    which provides per-db stats as well as global stats.
    
    Change-Id: I1eba19a3a0210d3127acbad119dfd2918414ff4a

--- puppet/modules/site_check_mk/manifests/agent/couchdb.pp
+++ puppet/modules/site_check_mk/manifests/agent/couchdb.pp
@@ -45,4 +45,11 @@ class site_check_mk::agent::couchdb {
       require => File['/etc/check_mk/mrpe.cfg'];
   }
 
+
+  # check different couchdb stats
+  file { '/usr/lib/check_mk_agent/local/leap_couch_stats.sh':
+    source  => 'puppet:///modules/site_check_mk/agent/local_checks/couchdb/leap_couch_stats.sh',
+    mode    => '0755',
+    require => Package['check_mk-agent']
+  }
 }
