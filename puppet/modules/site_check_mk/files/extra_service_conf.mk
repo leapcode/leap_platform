c9a9132d107d99a2f303fa766dae87397ac18274
elijah <elijah@riseup.net>
2015-03-04 10:58:24 -0800

    temporarily increase the delay between soledad / web api tests to 60 minutes, until we are able to fix the issue with the test users creating db bloat.

--- puppet/modules/site_check_mk/files/extra_service_conf.mk
+++ puppet/modules/site_check_mk/files/extra_service_conf.mk
@@ -1,13 +1,18 @@
 # retry 3 times before setting a service into a hard state
 # and send out notification
-extra_service_conf["max_check_attempts"] = [ 
-  ("4", ALL_HOSTS , ALL_SERVICES ) 
+extra_service_conf["max_check_attempts"] = [
+  ("4", ALL_HOSTS , ALL_SERVICES )
 ]
 
-# run check_mk_agent every 2 minutes if it terminates
-# successfully.
+#
+# run check_mk_agent every 2 minutes if it terminates successfully.
 # see https://leap.se/code/issues/6539 for the rationale
+#
+# update: temporarily set interval to 60 minutes until we solve the
+#         issue with the users db getting bloated with deleted
+#         test users.
+#
 extra_service_conf["normal_check_interval"] = [
-  ("2", ALL_HOSTS , "Check_MK" )
+  ("60", ALL_HOSTS , "Check_MK" )
 ]
 
