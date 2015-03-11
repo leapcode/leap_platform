b973fa14258ea8101433a325554ad8445b0c8820
varac <varacanero@zeromail.org>
2015-03-11 22:29:26 +0100

    Change nagios to be aware of soledad user change (Bug #6612)
    Change-Id: Id53d6432a58006653f4d9ddd6355ae505a5273eb

--- puppet/modules/site_check_mk/manifests/agent/soledad.pp
+++ puppet/modules/site_check_mk/manifests/agent/soledad.pp
@@ -11,6 +11,6 @@ class site_check_mk::agent::soledad {
     lens    => 'Spacevars.lns',
     changes => [
       'rm /files/etc/check_mk/mrpe.cfg/Soledad_Procs',
-      'set Soledad_Procs \'/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -a "/usr/bin/python /usr/bin/twistd --pidfile=/var/run/soledad.pid --logfile=/var/log/soledad.log web --wsgi=leap.soledad.server.application"\'' ]
+      'set Soledad_Procs \'/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -a "/usr/bin/python /usr/bin/twistd --uid=soledad --gid=soledad --pidfile=/var/run/soledad.pid --logfile=/var/log/soledad.log web --wsgi=leap.soledad.server.application --port=ssl:2323:privateKey=/etc/x509/keys/leap.key:certKey=/etc/x509/certs/leap.crt:sslmethod=SSLv23_METHOD"\'' ]
   }
 }
