caa62d845bbc54737aa40931c0b2eca068592dd9
varac <varacanero@zeromail.org>
2015-04-26 11:13:28 +0200

    Exec[] should log the output of the command on failure #6851
    Change-Id: If99ae4036f9210516762f5cd7eb66744661a9665

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,10 @@
 # set a default exec path
-Exec { path => '/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin' }
+# the logoutput exec parameter defaults to "on_error" in puppet 3,
+# but to "false" in puppet 2.7, so we need to set this globally here
+Exec {
+    logoutput => on_failure,
+      path    => '/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin'
+}
 
 include site_config::setup
 include site_config::default
