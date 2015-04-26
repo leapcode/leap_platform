eaddcfca6761bac329a57a65453958b6741f2420
varac <varacanero@zeromail.org>
2015-04-26 22:41:29 +0200

    run check_mk_agent every 4 instead of 10 minutes, useful for better graphs
    Change-Id: Ibefc6ce08cf714cf79a460a8b6eb32e2851ce22c

--- puppet/modules/site_check_mk/files/extra_service_conf.mk
+++ puppet/modules/site_check_mk/files/extra_service_conf.mk
@@ -5,10 +5,10 @@ extra_service_conf["max_check_attempts"] = [
 ]
 
 #
-# run check_mk_agent every 10 minutes if it terminates successfully.
+# run check_mk_agent every 4 minutes if it terminates successfully.
 # see https://leap.se/code/issues/6539 for the rationale
 #
 extra_service_conf["normal_check_interval"] = [
-  ("10", ALL_HOSTS , "Check_MK" )
+  ("4", ALL_HOSTS , "Check_MK" )
 ]
 
