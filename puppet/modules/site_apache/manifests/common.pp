f8b2a72aa09fca368d3038efa9100a48126f8ae9
Micah Anderson <micah@leap.se>
2015-09-15 11:52:20 -0400

    Fix server-status availability to tor hidden services (#7456)
    Make the server-status information unavailable by putting the vhost on a
    port that isn't configured as available to the tor hidden-service.
    
    Change-Id: Idd3bfefb5b7fc26fb0a8cf48cdf6afc68a4192bb

--- puppet/modules/site_apache/manifests/common.pp
+++ puppet/modules/site_apache/manifests/common.pp
@@ -1,27 +1,8 @@
 class site_apache::common {
-  # installs x509 cert + key and common config
-  # that both nagios + leap webapp use
-
-  $web_domain       = hiera('domain')
-  $domain_name      = $web_domain['name']
-
-  include x509::variables
-  include site_config::x509::commercial::cert
-  include site_config::x509::commercial::key
-  include site_config::x509::commercial::ca
-
-  Class['Site_config::X509::Commercial::Key'] ~> Service[apache]
-  Class['Site_config::X509::Commercial::Cert'] ~> Service[apache]
-  Class['Site_config::X509::Commercial::Ca'] ~> Service[apache]
 
   include site_apache::module::rewrite
 
   class { '::apache': no_default_site => true, ssl => true }
 
-  apache::vhost::file {
-    'common':
-      content => template('site_apache/vhosts.d/common.conf.erb')
-  }
-
-  apache::config::include{ 'ssl_common.inc': }
+  include site_apache::common::tls
 }
