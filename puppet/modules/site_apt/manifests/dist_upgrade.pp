5825513bcd14efd39031ba5cab3613a39c3027ec
varac <varacanero@zeromail.org>
2015-12-09 17:06:59 +0100

    [feat] Remove double run of apt-get update

--- puppet/modules/site_apt/manifests/dist_upgrade.pp
+++ puppet/modules/site_apt/manifests/dist_upgrade.pp
@@ -3,15 +3,11 @@ class site_apt::dist_upgrade {
   if $::apt_running == 'true' {
     fail ('apt-get is running in background - Please wait until it finishes. Exiting.')
   } else {
-    exec{'initial_apt_update':
-      command     => '/usr/bin/apt-get update',
-      refreshonly => false,
-      timeout     => 360,
-    }
     exec{'initial_apt_dist_upgrade':
       command     => "/usr/bin/apt-get -q -y -o 'DPkg::Options::=--force-confold'  dist-upgrade",
       refreshonly => false,
       timeout     => 1200,
+      require     => Exec['refresh_apt']
     }
   }
 }
