3bb37824d5cf1e146a257d14d5cf41d7df3def5d
varac <varacanero@zeromail.org>
2015-12-09 17:13:23 +0100

    [style] Lint site_apt::dist_upgrade

--- puppet/modules/site_apt/manifests/dist_upgrade.pp
+++ puppet/modules/site_apt/manifests/dist_upgrade.pp
@@ -1,6 +1,10 @@
+# upgrade all packages
 class site_apt::dist_upgrade {
 
+  # facter returns 'true' as string
+  # lint:ignore:quoted_booleans
   if $::apt_running == 'true' {
+  # lint:endignore
     fail ('apt-get is running in background - Please wait until it finishes. Exiting.')
   } else {
     exec{'initial_apt_dist_upgrade':
