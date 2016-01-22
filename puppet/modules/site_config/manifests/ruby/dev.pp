38e1003186ca194c75b87fdb8898304556d89a7a
varac <varacanero@zeromail.org>
2016-01-22 23:52:58 +0100

    [bug] Use ruby::devel to install ruby-dev deb
    Ruby itself is a parameterized class, and parameters cannot get
    overridden (see https://projects.puppetlabs.com/issues/9259).
    
    The webapp node didn't install the ruby-dev package (we never
    noticed because our vagrant images as probably other debian images
    had ruby-dev preinstalled).
    
    We now use the ruby::devel class to install ruby-dev.
    
    - Tested: [citest-jessie]
    - Resolves: #7838

--- puppet/modules/site_config/manifests/ruby/dev.pp
+++ puppet/modules/site_config/manifests/ruby/dev.pp
@@ -1,8 +1,8 @@
 # install ruby dev packages needed for building some gems
-class site_config::ruby::dev inherits site_config::ruby {
-  Class['::ruby'] {
-    install_dev  => true
-  }
+class site_config::ruby::dev {
+  include site_config::ruby
+  include ::ruby::devel
+
   # building gems locally probably requires build-essential and gcc:
   include site_config::packages::build_essential
 }
