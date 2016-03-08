aa2c2590e7b976805402c4c0a2ebe4b554304a85
varac <varacanero@zeromail.org>
2016-03-08 13:51:32 +0100

    [bug] Remove stunnel leftovers from bigcouch
    - Resolves: #7785

--- puppet/modules/site_config/manifests/remove/bigcouch.pp
+++ puppet/modules/site_config/manifests/remove/bigcouch.pp
@@ -18,4 +18,25 @@ class site_config::remove::bigcouch {
   cron { 'compact_all_shards':
     ensure => absent
   }
+
+
+  exec { 'kill_bigcouch_stunnel_procs':
+    refreshonly => true,
+    command     => '/usr/bin/pkill -f "/usr/bin/stunnel4 /etc/stunnel/(ednp|epmd)_server.conf"'
+  }
+
+  # 'tidy' doesn't notify other resources, so we need to use file here instead
+  # see https://tickets.puppetlabs.com/browse/PUP-6021
+  file {
+    [ '/etc/stunnel/ednp_server.conf', '/etc/stunnel/epmd_server.conf']:
+      ensure => absent,
+      # notifying Service[stunnel] doesn't work here because the config
+      # files contain the pid of the procs to stop/start.
+      # If we remove the config, and restart stunnel then it will only
+      # stop/start the procs for which config files are found and the stale
+      # service will continue to run.
+      # So we simply kill them.
+      notify => Exec['kill_bigcouch_stunnel_procs']
+  }
+
 }
