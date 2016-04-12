aec082688b8c20b5cd549feb81d1003847d5f744
elijah <elijah@riseup.net>
2016-04-12 11:07:26 -0700

    leap_cli: rename 'debug' command to 'info' command, since --debug is already a thing.

--- lib/leap_cli/commands/info.rb
+++ lib/leap_cli/commands/info.rb
@@ -1,9 +1,9 @@
 module LeapCli; module Commands
 
-  desc 'Output debug information.'
+  desc 'Prints information regarding facts, history, and running processes for a node or nodes.'
   long_desc 'The FILTER can be the name of a node, service, or tag.'
   arg_name 'FILTER'
-  command [:debug, :d] do |c|
+  command [:info] do |c|
     c.action do |global,options,args|
       nodes = manager.filter!(args)
       ssh_connect(nodes, connect_options(options)) do |ssh|
