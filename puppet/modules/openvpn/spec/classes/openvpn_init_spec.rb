d6719731dce8ee7e048a16a447a426abcaa44f24
elijah <elijah@riseup.net>
2016-07-21 12:13:24 -0700

    remove openvpn submodule

--- puppet/modules/openvpn/spec/classes/openvpn_init_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
- 
-describe 'openvpn', :type => :class do
-
-  let (:facts) { { :concat_basedir => '/var/lib/puppet/concat' } }
-
-  it { should create_class('openvpn') }
-
-end
