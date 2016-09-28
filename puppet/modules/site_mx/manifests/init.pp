291d5718607a2df22ca58a0c110bee75452a085c
varac <varacanero@zeromail.org>
2016-09-28 11:01:19 +0200

    lint site_mx class

--- puppet/modules/site_mx/manifests/init.pp
+++ puppet/modules/site_mx/manifests/init.pp
@@ -1,20 +1,21 @@
+# Configure leap_mx on mx server
 class site_mx {
   tag 'leap_service'
-  Class['site_config::default'] -> Class['site_mx']
+  Class['::site_config::default'] -> Class['::site_mx']
 
-  include site_config::default
-  include site_config::x509::cert
-  include site_config::x509::key
-  include site_config::x509::ca
-  include site_config::x509::client_ca::ca
-  include site_config::x509::client_ca::key
+  include ::site_config::default
+  include ::site_config::x509::cert
+  include ::site_config::x509::key
+  include ::site_config::x509::ca
+  include ::site_config::x509::client_ca::ca
+  include ::site_config::x509::client_ca::key
 
-  include site_stunnel
+  include ::site_stunnel
 
-  include site_postfix::mx
-  include site_haproxy
-  include site_shorewall::mx
-  include site_shorewall::service::smtp
-  include leap_mx
-  include site_check_mk::agent::mx
+  include ::site_postfix::mx
+  include ::site_haproxy
+  include ::site_shorewall::mx
+  include ::site_shorewall::service::smtp
+  include ::leap_mx
+  include ::site_check_mk::agent::mx
 }
