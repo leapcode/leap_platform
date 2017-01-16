5126584fe40f08b779a6f93e2d1b6b57bae29700
Tulio Casagrande <tcasagra@thoughtworks.com>
2017-01-16 15:15:41 -0200

    Add apache auto-restart extension file

---
+++ puppet/modules/site_apache/manifests/common/extensions.pp
@@ -0,0 +1,14 @@
+class site_apache::common::extensions {
+
+  include ::systemd
+  file { '/etc/systemd/system/apache2.service.d/auto_restart.conf':
+    source  => 'puppet:///modules/site_apache/auto_restart.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => [
+      Service['apache']
+    ]
+  }~>
+  Exec['systemctl-daemon-reload']
+}
