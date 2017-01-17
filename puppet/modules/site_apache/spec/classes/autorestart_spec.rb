80e0ca380e92fe435622dbd35d1a5baedb6c3f92
Tulio Casagrande <tcasagra@thoughtworks.com>
2017-01-17 15:18:30 -0200

    Ensure the directory exists before creating the file
    with @aarni

---
+++ puppet/modules/site_apache/spec/classes/autorestart_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'site_apache::common::autorestart' do
+  it "should include apache autorestart" do
+    should contain_file('apache2.service.d/autorestart.conf').with_source('puppet:///modules/site_apache/autorestart.conf')
+  end
+end
