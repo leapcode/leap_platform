b6e19e290eb6395ce0a12bf2307282b00e7456ea
Tulio Casagrande <tcasagra@thoughtworks.com>
2017-01-17 14:26:27 -0200

    Change autorestart to use systemd::unit_file

--- puppet/modules/site_apache/manifests/common/autorestart.pp
+++ puppet/modules/site_apache/manifests/common/autorestart.pp
@@ -3,13 +3,8 @@
 #
 class site_apache::common::autorestart {
 
-  include ::systemd
-  file { '/etc/systemd/system/apache2.service.d/autorestart.conf':
+  ::systemd::unit_file { '/etc/systemd/system/apache2.service.d/autorestart.conf':
     source  => 'puppet:///modules/site_apache/autorestart.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     require => Service['apache'],
-    notify  => Exec['systemctl-daemon-reload']
   }
 }
