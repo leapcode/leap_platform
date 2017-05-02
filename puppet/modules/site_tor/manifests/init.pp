449d6169b8d0c7f31279b445f5dd103b244d7382
Micah Anderson <micah@riseup.net>
2017-05-02 16:24:15 -0400

    Install tor from backports (fixes #8783).
    The newer version is needed for the single-hop functionality.

--- puppet/modules/site_tor/manifests/init.pp
+++ puppet/modules/site_tor/manifests/init.pp
@@ -1,6 +1,14 @@
 # generic configuration needed for tor
 class site_tor {
 
+  # Ensure the tor version is the latest from backports
+  # see https://0xacab.org/leap/platform/issues/8783
+  apt::preferences_snippet { 'tor':
+    package  => 'tor',
+    release  => "${::lsbdistcodename}-backports",
+    priority => 999,
+    before   => Class['tor::daemon']  }
+
   class { 'tor::daemon': ensure_version => latest }
 
 }
