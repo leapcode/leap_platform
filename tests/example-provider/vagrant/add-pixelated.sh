04a1c0bc3ed7173836ed790776be1a13437310a7
Tulio Casagrande <tcasagra@thoughtworks.com>
2017-05-10 14:43:46 -0300

    Remove pixelated submodule from the example provider

--- tests/example-provider/vagrant/add-pixelated.sh
+++ tests/example-provider/vagrant/add-pixelated.sh
@@ -6,8 +6,8 @@
 
 cd "$PROVIDERDIR"
 
-if ! git submodule status files/puppet/modules/pixelated > /dev/null 2>&1; then
-  git submodule add https://github.com/pixelated/puppet-pixelated.git files/puppet/modules/pixelated
+if ! [ -d files/puppet/modules/pixelated ]; then
+  git clone https://github.com/pixelated/puppet-pixelated.git files/puppet/modules/pixelated
 fi
 
 echo '{}' > services/pixelated.json
@@ -26,7 +26,7 @@ $LEAP $OPTS -v 2 test --continue
 echo -e '\n===========================================================================================================\n\n'
 echo -e 'You are now ready to use your vagrant Pixelated provider.\n'
 
-echo -e 'The LEAP webapp is available at https://localhost:4443. Use it to register an account before using the Pixelated Useragent.\n'
-echo -e 'The Pixelated Useragent is available at https://localhost:8080\n'
+echo -e 'The LEAP webapp is available at https://localhost:4443. Use it to register an account before using the Pixelated User Agent.\n'
+echo -e 'The Pixelated User Agent is available at https://localhost:8080\n'
 
 echo -e 'Please add an exception for both sites in your browser dialog to allow the self-signed certificate.\n'
