40f7b49003594a1be8c0540a92292d7cfb63eb61
varac <varacanero@zeromail.org>
2017-05-23 13:58:38 +0200

    [vagrant] Don't block eth0 if eth1 is configured
    Eth0 is vagrant's main interface to access the box

--- puppet/modules/site_config/manifests/vagrant.pp
+++ puppet/modules/site_config/manifests/vagrant.pp
@@ -2,10 +2,14 @@
 class site_config::vagrant {
 
   include site_shorewall::defaults
-  # eth0 on vagrant nodes is the uplink
-  shorewall::interface { 'eth0':
-    zone    => 'net',
-    options => 'tcpflags,blacklist,nosmurfs';
+
+  if ( $::site_config::params::interface == 'eth1' ) {
+    # Don't block eth0 even if eth1 is configured, because
+    # it's vagrant's main interface to access the box
+    shorewall::interface { 'eth0':
+      zone    => 'net',
+      options => 'tcpflags,blacklist,nosmurfs';
+    }
   }
 
 }
