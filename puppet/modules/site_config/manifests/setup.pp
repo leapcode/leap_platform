d2824a6bc1178c6c2ce4923faacfde8e05f8389a
varac <varacanero@zeromail.org>
2017-05-23 13:56:57 +0200

    Include site_config::vagrant on vagrant nodes

--- puppet/modules/site_config/manifests/setup.pp
+++ puppet/modules/site_config/manifests/setup.pp
@@ -37,7 +37,7 @@ class site_config::setup {
   # we need to include shorewall::interface{eth0} in setup.pp so
   # packages can be installed during main puppetrun, even before shorewall
   # is configured completly
-  if ( $::site_config::params::environment == 'local' ) {
+  if $::vagrant {
     include site_config::vagrant
   }
 
