de892c3821b2053f23ca6a91bf097b05a150ff41
elijah <elijah@riseup.net>
2017-06-28 13:23:02 -0700

    static - gracefully handle incorrect static site configs

--- provider_base/services/static.rb
+++ provider_base/services/static.rb
@@ -1,2 +1,4 @@
-self['dns']['aliases'] += self.static.domains.keys
-self['dns']['aliases'].uniq!
+if self['static'] && self['static']['domains']
+  self['dns']['aliases'] += self['static']['domains'].keys
+  self['dns']['aliases'].uniq!
+end
