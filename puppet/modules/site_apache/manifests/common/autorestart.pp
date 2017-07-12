f9ff6afebde2fe037fc9a90928501ff1a8d4ccde
Micah Anderson <micah@riseup.net>
2017-07-12 13:29:06 -0700

    bug: Set .placeholder to fix removal
    Add a .placeholder file so the directory doesn't get removed by
    deb-systemd-helper when a package runs a purge in its postrm. This is a
    work-around and fixes #8841. It probably wont be needed post-jessie.

--- puppet/modules/site_apache/manifests/common/autorestart.pp
+++ puppet/modules/site_apache/manifests/common/autorestart.pp
@@ -3,11 +3,21 @@
 #
 class site_apache::common::autorestart {
 
-  file { '/etc/systemd/system/apache2.service.d':
-    ensure => directory,
-    owner  => root,
-    group  => root,
-    mode   => '0755',
+  file {
+    '/etc/systemd/system/apache2.service.d':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755';
+
+    # Add .placeholder file so directory doesn't get removed by
+    # deb-systemd-helper in a package removal postrm, see
+    # issue #8841 for more details.
+    '/etc/systemd/system/apache2.service.d/.placeholder':
+      ensure => file,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755';
   }
 
   ::systemd::unit_file { 'apache2.service.d/autorestart.conf':
